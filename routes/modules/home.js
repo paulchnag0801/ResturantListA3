8029907af6ff721d980264cb459b1218d23d8a0a
paulchnag0801 <x1232178987@gmail.com>
2021-07-13 15:29:56 +0800

    refactor: modify express routes

---
+++ routes/modules/home.js
@@ -0,0 +1,16 @@
+// 引用 Express 與 Express 路由器
+const express = require('express')
+const router = express.Router()
+
+const Restaurant = require('../../models/restaurant') //載入restaurant model
+
+// routes setting
+router.get('/', (req, res) => {
+  Restaurant.find()
+    .lean()
+    .then((restaurants) => res.render('index', { restaurants }))
+    .catch((error) => console.error(error))
+})
+
+// 匯出路由模組
+module.exports = router
---
+++ routes/modules/restaurants.js
@@ -0,0 +1,140 @@
+const express = require('express')
+const router = express.Router()
+const Restaurant = require('../../models/restaurant') //載入restaurant model
+const mongoose = require('mongoose')
+
+router.get('/searches', (req, res) => {
+  const keyword = req.query.keyword.trim().toLowerCase()
+  if (keyword <= 0) {
+    return res.redirect('/')
+  }
+  Restaurant.find()
+    .lean()
+    .then((restaurants) => {
+      restaurants = restaurants.filter(
+        (restaurant) =>
+          restaurant.name.toLowerCase().includes(keyword) ||
+          restaurant.category.includes(keyword)
+      )
+      if (restaurants.length > 0) {
+        return res.render('index', {
+          restaurants: restaurants,
+          keyword: req.query.keyword.trim(),
+        })
+      } else {
+        res.render('index', {
+          keyword: req.query.keyword,
+          no_result: `<h3> 沒有"${keyword}"的搜尋結果，請輸入正確的餐廳名稱</h3>`,
+        })
+      }
+    })
+    .catch((error) => console.error(error))
+})
+
+// create route
+router.get('/new', (req, res) => {
+  res.render('new')
+})
+
+router.post('/', (req, res) => {
+  const {
+    name,
+    name_en,
+    category,
+    image,
+    location,
+    phone,
+    google_map,
+    rating,
+    description,
+  } = req.body
+  if (
+    !name ||
+    !category ||
+    !image ||
+    !location ||
+    !phone ||
+    !google_map ||
+    !rating ||
+    !description
+  ) {
+    return res.redirect('/restaurants/new')
+  }
+  return Restaurant.create({
+    name,
+    name_en,
+    category,
+    image,
+    location,
+    phone,
+    google_map,
+    rating,
+    description,
+  })
+    .then(() => res.redirect('/'))
+    .catch((error) => console.error(error))
+})
+
+// read detail route
+router.get('/:id', (req, res) => {
+  const id = req.params.id
+  if (!mongoose.Types.ObjectId.isValid(id)) return res.redirect('back')
+  return Restaurant.findById(id)
+    .lean()
+    .then((restaurant) => res.render('detail', { restaurant }))
+    .catch((error) => console.error(error))
+})
+
+// Update edit route
+router.get('/:id/edit', (req, res) => {
+  const id = req.params.id
+  if (!mongoose.Types.ObjectId.isValid(id)) return res.redirect('back')
+  return Restaurant.findById(id)
+    .lean()
+    .then((restaurant) => res.render('edit', { restaurant }))
+    .catch((error) => console.error(error))
+})
+
+router.put('/:id', (req, res) => {
+  const id = req.params.id
+  if (!mongoose.Types.ObjectId.isValid(id)) return res.redirect('back')
+  const editRestaurant = req.body
+  return Restaurant.findById(id)
+    .then((restaurant) => {
+      restaurant.name = editRestaurant.name
+      restaurant.name_en = editRestaurant.name_en
+      restaurant.category = editRestaurant.category
+      restaurant.image = editRestaurant.image
+      restaurant.location = editRestaurant.location
+      restaurant.phone = editRestaurant.phone
+      restaurant.google_map = editRestaurant.google_map
+      restaurant.rating = editRestaurant.rating
+      restaurant.description = editRestaurant.description
+      return restaurant.save()
+    })
+    .then(() => res.redirect(`/restaurants/${id}`))
+    .catch((error) => console.error(error))
+})
+
+// delete route
+router.delete('/:id', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .then((restaurant) => restaurant.remove())
+    .then(() => res.redirect('/'))
+    .catch((error) => console.log(error))
+})
+
+// sort restaurants
+// sort
+router.get('/sort', (req, res) => {
+  const { select } = req.query
+  
+  Restaurant.find()
+    .lean()
+    .sort(select)
+    .then(restaurants => res.render('index', { restaurants, select }))
+    .catch(error => console.log(error))
+})
+
+module.exports = router
