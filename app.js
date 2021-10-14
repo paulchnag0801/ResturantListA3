6a9a63e81d4996001801b7c386a874644613e663
paulchnag0801 <x1232178987@gmail.com>
2021-10-14 16:09:34 +0800

    feat: add message partials

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express')
 const session = require('express-session')
 const methodOverride = require('method-override')
+const flash = require('connect-flash')
 
 // 引用路由器
 const routes = require('./routes')
@@ -43,10 +44,13 @@ app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
 usePassport(app)
+app.use(flash()) 
 
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg') 
   next()
 })
 
