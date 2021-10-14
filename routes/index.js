83990787f7ec3e26c10ca23ce3af49cb95e1403b
paulchnag0801 <x1232178987@gmail.com>
2021-10-14 12:19:22 +0800

    feat: add user modules

--- routes/index.js
+++ routes/index.js
@@ -10,8 +10,10 @@ router.use('/', home)
 
 // 引入 restaurants 模組程式碼
 const restaurants = require('./modules/restaurants')
+const users = require('./modules/users')
 // 將網址結構符合 /restaurants 字串開頭的 request 導向 restaurants 模組
 router.use('/restaurants', restaurants)
+router.use('/users', users)
 
 // 匯出路由器
 module.exports = router
